2bbac9d68fcdb74e5822b2ce6cd74f203be09886
JulyP <u20211d760@upc.edu.pe>
2025-05-03 05:11:32 -0500

    feat(article): add article entity.

---
+++ src/news/model/article.entity.js
@@ -0,0 +1,42 @@
+import {Source} from "./source.entity.js";
+
+export class Article {
+    constructor({
+                    author = '',
+                    title = '',
+                    description = '',
+                    url = '',
+                    urlToImage = '',
+                    source = null,
+                    publishedAt = ''}) {
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.source = source ? new Source(source) : null;
+        this.publishedAt = new Date(publishedAt);
+    }
+
+    getFormattedPublishedAt() {
+        return this.publishedAt.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit'});
+    }
+
+    getFullFormattedPublishedAt() {
+        let dateAsString = this.publishedAt.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit'});
+        let timeAsString = this.publishedAt.toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'});
+        return `on ${dateAsString} at ${timeAsString}`;
+    }
+
+}
